70c756f8ae73dc8e10a874a09b42a1737071f540
tu10ng <2059734099@qq.com>
2023-02-27 16:57:04 +0800

    (repl error) finish

--- cllox.lisp
+++ cllox.lisp
@@ -1,16 +1,43 @@
 (require "uiop")
 
+
+
+;; error
 (defmacro handling-errors (&body body)
-  `(handler-case (progn ,@body)
-     (simple-condition (err) 
-       (format *error-output* "~&~A: ~%" (class-name (class-of err)))
-       (apply (function format) *error-output*
-              (simple-condition-format-control   err)
-              (simple-condition-format-arguments err))
-       (format *error-output* "~&"))
-     (condition (err) 
-       (format *error-output* "~&~A: ~%  ~S~%"
-               (class-name (class-of err)) err))))
+  "https://www.cliki.net/REPL"
+  `(handler-bind
+       ((run-error
+          #'(lambda (e)
+              (format *error-output*
+                      "got error: ~a~%" e)
+              (invoke-restart 'ignore)))
+        (simple-condition
+          #'(lambda (err)
+              (format *error-output* "~&~A: ~%" (class-name (class-of err)))
+              (apply (function format) *error-output*
+                     (simple-condition-format-control   err)
+                     (simple-condition-format-arguments err))
+              (format *error-output* "~&")
+              (exit)))
+        (sb-sys:interactive-interrupt
+          #'(lambda (err)
+              (format *error-output* "~&~A: ~%  ~S~%"
+                      (class-name (class-of err)) err)
+              (exit))))
+     (progn ,@body)))
+
+
+(define-condition run-error (error)
+  ((reason :initarg :reason :reader run-error-reason)
+   (line :initarg :line :reader run-error-line)
+   (source :initarg :source :reader run-error))
+  (:documentation "signaled during running code???")
+  (:report (lambda (c s)
+             (with-slots (reason line source)
+                 c
+               (format s "~a on line ~a~@[ of ~a~]" reason line source)))))
+
+
 
 (defun main ()
   (let* ((args (cdr sb-ext:*posix-argv*))
@@ -27,21 +54,23 @@
   (do ((hist 1 (1+ hist)))
       (nil)
     (format t "~%lox[~a]> " hist)
+    (finish-output)
     (handling-errors
-     (finish-output)
      (let ((line (read-line)))
-       (when (member line
-                     '("quit" "exit" "")
+       (when (member line '("quit" "exit" "")
                      :test #'string=)
          (return-from run-repl))
        (run line)))))
 
 (defun run (src)
-  (format t "~a~%" src))
+  (restart-case
+      (progn
+        (format t "~a~%" src))
+    (ignore ()
+      :report "ignore error in running"
+      nil)))
 
-(defun print-error (line file message)
-  (format *error-output* "[~a] Error ~a: ~a" line file message)) 
-
+
 
 
 (main)
